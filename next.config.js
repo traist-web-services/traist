062ca9bf26ebed45ce0f3a05eb392ea4d4745731
Joe Innes <joe@joeinn.es>
2021-01-19 09:45:15 +0100

    Added security headers

--- next.config.js
+++ next.config.js
@@ -1,12 +1,37 @@
 module.exports = {
   images: {
-    domains: ["source.unsplash.com"]
+    domains: ["source.unsplash.com"],
   },
-  webpack: function(config) {
+  webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
-      use: "raw-loader"
+      use: "raw-loader",
     });
     return config;
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*?)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1",
+          },
+        ],
+      },
+    ];
+  },
 };
